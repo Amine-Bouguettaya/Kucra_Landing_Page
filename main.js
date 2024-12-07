9b176592eecb7082f3b6632597c72f0801ca833e
Amine-Bouguettaya <aminebouguettaya5@gmail.com>
2024-12-07 11:47:00 +0000

    add color change for every screen, color change to local storage (only 1 color for now)

--- main.js
+++ main.js
@@ -26,38 +26,90 @@ function myFunction() {
     x.style.display = "flex";
   }
 }
-function backgroundColorChange() {
+
+if (localStorage.getItem("active")) {
   let bg = document.getElementById("scrollTop");
   let submitButton = document.getElementById("submitButton");
   let card = document.getElementsByClassName("card");
   let learnbutton = document.getElementsByClassName("learnButton");
   let valueCards = document.getElementsByClassName("valueCard");
-  let titleContainer = document.getElementsByClassName("titleContainer");
-  console.log(titleContainer);
-  console.log(valueCards);
-  if (bg.classList.contains("active")) {
-    submitButton.style.backgroundColor = "#2596be";
-    bg.style.background = "radial-gradient(circle, rgba(139,195,245,1) 35%, rgba(18,148,184,1) 100%)";
-    learnbutton[0].style.background = "608cfc";
-    titleContainer[0].style.background = "608cfc";
-    for (var i = 0; i < card.length; i++) {
-      card[i].style.background = "radial-gradient(circle, rgba(139,195,245,1) 35%, rgba(18,148,184,1) 100%)";
-    }
-    for (var i = 0; i < valueCards.length; i++) {
-      valueCards[i].style.color = "#608cfc";
-    }
-    bg.classList.remove("active");
-  } else {
+  let titleContainer = document.getElementById("promoCard");
+  let joinButton = document.getElementsByClassName("joinButton");
+  let promoButton = document.getElementsByClassName("promoButton");
+  let readMore = document.getElementsByClassName("readMore");
+
     submitButton.style.backgroundColor = "#be2525";
-    for (var i = 0; i < card.length; i++) {
+  for (var i = 0; i < card.length; i++) {
     card[i].style.background = "radial-gradient(circle, #f58b8b 35%, #b81212 100%)";
-    }
-    for (var i = 0; i < valueCards.length; i++) {
-      valueCards[i].style.color = "#fc6060";
-    }
-    bg.style.background = "radial-gradient(circle, #f58b8b 35%, #b81212 100%)";
-    learnbutton[0].style.background = "#fc6060";
-    titleContainer[0].style.background = "#fc6060";
-    bg.classList.add("active");
   }
+  for (var i = 0; i < valueCards.length; i++) {
+    valueCards[i].style.color = "#fc6060";
+  }
+  for (var i = 0; i < readMore.length; i++) {
+    readMore[i].style.color = "#fc6060";
+  }
+  for (var i = 0; i < joinButton.length; i++) {
+    joinButton[i].style.border = "1px solid #fc6060"
+    joinButton[i].style.color = "#fc6060";
+  }
+  bg.style.background = "radial-gradient(circle, #f58b8b 35%, #b81212 100%)";
+  learnbutton[0].style.background = "#fc6060";
+  promoButton[0].style.background = "#fc6060";
+  titleContainer.style.background = "#fc6060";
+  bg.classList.add("active");
 }
+function backgroundColorChange() {
+    let bg = document.getElementById("scrollTop");
+    let submitButton = document.getElementById("submitButton");
+    let card = document.getElementsByClassName("card");
+    let learnbutton = document.getElementsByClassName("learnButton");
+    let valueCards = document.getElementsByClassName("valueCard");
+    let titleContainer = document.getElementById("promoCard");
+    let joinButton = document.getElementsByClassName("joinButton");
+    let promoButton = document.getElementsByClassName("promoButton");
+    let readMore = document.getElementsByClassName("readMore");
+  
+  if (bg.classList.contains("active")) {
+      submitButton.style.backgroundColor = "#2596be";
+      bg.style.background = "radial-gradient(circle, rgba(139,195,245,1) 35%, rgba(18,148,184,1) 100%)";
+      learnbutton[0].style.background = "608cfc";
+      titleContainer.style.background = "608cfc";
+      for (var i = 0; i < card.length; i++) {
+        card[i].style.background = "radial-gradient(circle, rgba(139,195,245,1) 35%, rgba(18,148,184,1) 100%)";
+      }
+      for (var i = 0; i < valueCards.length; i++) {
+        valueCards[i].style.color = "#608cfc";
+      }
+      for (var i = 0; i < joinButton.length; i++) {
+        joinButton[i].style.border = "1px solid #608cfc;";
+        joinButton[i].style.color = "#608cfc";
+      }
+      for (var i = 0; i < readMore.length; i++) {
+        readMore[i].style.color = "#608cfc";
+      }
+      promoButton[0].style.background = "608cfc";
+      bg.classList.remove("active");
+      localStorage.removeItem("active");
+    } else {
+        submitButton.style.backgroundColor = "#be2525";
+      for (var i = 0; i < card.length; i++) {
+        card[i].style.background = "radial-gradient(circle, #f58b8b 35%, #b81212 100%)";
+      }
+      for (var i = 0; i < valueCards.length; i++) {
+        valueCards[i].style.color = "#fc6060";
+      }
+      for (var i = 0; i < readMore.length; i++) {
+        readMore[i].style.color = "#fc6060";
+      }
+      for (var i = 0; i < joinButton.length; i++) {
+        joinButton[i].style.border = "1px solid #fc6060"
+        joinButton[i].style.color = "#fc6060";
+      }
+      bg.style.background = "radial-gradient(circle, #f58b8b 35%, #b81212 100%)";
+      learnbutton[0].style.background = "#fc6060";
+      promoButton[0].style.background = "#fc6060";
+      titleContainer.style.background = "#fc6060";
+      bg.classList.add("active");
+      localStorage.setItem("active", "color: Red");
+   }
+  }
